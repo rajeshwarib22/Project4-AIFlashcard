7c2a8ecfb6b043e0869daff1435429affaf8dcaa
Rajeshwari Bhirud <57486845+rajeshwarib22@users.noreply.github.com>
2024-08-23 19:17:16 -0400

    Update route.js
    api key changes

--- app/api/route.js
+++ app/api/route.js
@@ -30,7 +30,9 @@ You should return in the following JSON format:\
 }'
 
 export async function POST(req){
-    const openai = new OpenAI()
+    const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  })
     const data = await req.text()
 
     //Create chat completion request to OpenAI API
